33d7834ceb0196193b7c521aebf5d5de3c887fc5
Raphael Chookagian <raphaelchookagian@gmail.com>
2022-11-30 12:26:36 -0800

    adding to class 3 lab

--- src/server.js
+++ src/server.js
@@ -5,38 +5,18 @@ const express = require('express');
 const cors = require('cors');
 const notFound = require('./error-handlers/404');
 const errorHandler = require('./error-handlers/500');
-const { CustomerModel } = require('./models');
+// const { CustomerModel } = require('./models');
 const PORT = process.env.PORT || 3002;
+const customRouter = require('./routes/customers');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(customRouter);
 
 app.get('/', (req, res, next) => {
   res.status(200).send('Hello World!');
 });
-
-app.get('/customer', async (req, res, next) => {
-// const users = await User.findAll();
-  try {
-    const customers = await CustomerModel.findAll();
-    res.status(200).send(customers);
-  } catch (e) {
-    next(e);
-  }
-});
-
-app.post('/customer', async (req, res, next) => {
-  try {
-    const newCustomer = await CustomerModel.create(req.body);
-    res.status(200).send(newCustomer);
-
-  } catch (e) {
-    next(e);
-  }
-});
-
-
 
 app.use('*', notFound);
 app.use(errorHandler);
@@ -45,3 +25,26 @@ function start(){
   app.listen(PORT, () => console.log('listening on port', PORT));
 }
 module.exports = { app, start };
+
+// app.get('/customer', async (req, res, next) => {
+// // const users = await User.findAll();
+//   try {
+//     const customers = await CustomerModel.findAll();
+//     res.status(200).send(customers);
+//   } catch (e) {
+//     next(e);
+//   }
+// });
+
+// app.post('/customer', async (req, res, next) => {
+//   try {
+//     const newCustomer = await CustomerModel.create(req.body);
+//     res.status(200).send(newCustomer);
+
+//   } catch (e) {
+//     next(e);
+//   }
+// });
+
+
+
