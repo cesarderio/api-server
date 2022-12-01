1eff4d4c0e2b4059c9fb341a7faf007ec68a3cd4
Raphael Chookagian <raphaelchookagian@gmail.com>
2022-11-30 18:36:59 -0800

    working on adding tests to all three tables

--- README.md
+++ README.md
@@ -52,6 +52,17 @@ i.e.
   - handles errors
   handles invalid requests
 
+- 404 on a bad route
+- 404 on a bad method
+
+The correct status codes and returned data for each REST route:
+
+- Create a record using POST
+- Read a list of records using GET
+- Read a record using GET
+- Update a record using PUT
+- Destroy a record using DELETE
+
 ### Whiteboard
 
 #### UML
--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -41,6 +41,33 @@ describe('REST API', () => {
     expect(response.body[0].pronouns).toEqual('they/them');
   });
 
+  // test('finds a customer by id', async () => {
+  //   let response = await request.get('/customer/:id');
+
+  //   expect(response.status).toEqual(200);
+  //   expect(response.body[0].name).toEqual('tester');
+  //   expect(response.body[0].age).toEqual(42);
+  //   expect(response.body[0].pronouns).toEqual('they/them');
+  // });
+
+  // test('updates a customer by id', async () => {
+  //   let response = await request.update('/customer/:id');
+
+  //   expect(response.status).toEqual(200);
+  //   expect(response.body[0].name).toEqual('tester');
+  //   expect(response.body[0].age).toEqual(42);
+  //   expect(response.body[0].pronouns).toEqual('they/them');
+  // });
+
+  // test('deletes a customer by id', async () => {
+  //   let response = await request.destroy('/customer/:id');
+
+  //   expect(response.status).toEqual(200);
+  //   expect(response.body[0].name).toEqual('tester');
+  //   expect(response.body[0].age).toEqual(42);
+  //   expect(response.body[0].pronouns).toEqual('they/them');
+  // });
+
   test('Create an order', async () => {
     let response = await request.post('/order').send({
       productName: 'tester',
@@ -54,6 +81,8 @@ describe('REST API', () => {
     expect(response.body.shippingSpeed).toEqual('Standard');
   });
 
+
+
   test('finds all orders', async () => {
     let response = await request.get('/order');
 
@@ -62,6 +91,35 @@ describe('REST API', () => {
     expect(response.body[0].amount).toEqual(1);
     expect(response.body[0].shippingSpeed).toEqual('Standard');
   });
+
+  // test('finds a order by id', async () => {
+  //   let response = await request.get('/order/:id');
+
+  //   expect(response.status).toEqual(200);
+  //   expect(response.body[0].name).toEqual('tester');
+  //   expect(response.body[0].age).toEqual(42);
+  //   expect(response.body[0].pronouns).toEqual('they/them');
+  // });
+
+  // test('updates a order by id', async () => {
+  //   let response = await request.update('/customer/:id');
+
+  //   expect(response.status).toEqual(200);
+  //   expect(response.body[0].name).toEqual('tester');
+  //   expect(response.body[0].age).toEqual(42);
+  //   expect(response.body[0].pronouns).toEqual('they/them');
+  // });
+
+  // test('deletes a customer by id', async () => {
+  //   let response = await request.destroy('/customer/:id');
+
+  //   expect(response.status).toEqual(200);
+  //   expect(response.body[0].name).toEqual('tester');
+  //   expect(response.body[0].age).toEqual(42);
+  //   expect(response.body[0].pronouns).toEqual('they/them');
+  // });
+
+
 
   test('Create a new person', async () => {
     let response = await request.post('/people').send({
@@ -90,5 +148,33 @@ describe('REST API', () => {
     // expect(response.body[0].phone).toEqual(2065555555);
     // expect(response.body[0].email).toEqual('hello@world.com');
   });
+
+
+  // test('finds people by id', async () => {
+  //   let response = await request.get('/people/:id');
+
+  //   expect(response.status).toEqual(200);
+  //   expect(response.body[0].name).toEqual('tester');
+  //   expect(response.body[0].age).toEqual(42);
+  //   expect(response.body[0].pronouns).toEqual('they/them');
+  // });
+
+  // test('updates people by id', async () => {
+  //   let response = await request.update('/customer/:id');
+
+  //   expect(response.status).toEqual(200);
+  //   expect(response.body[0].name).toEqual('tester');
+  //   expect(response.body[0].age).toEqual(42);
+  //   expect(response.body[0].pronouns).toEqual('they/them');
+  // });
+
+  // test('deletes people by id', async () => {
+  //   let response = await request.destroy('/customer/:id');
+
+  //   expect(response.status).toEqual(200);
+  //   expect(response.body[0].name).toEqual('tester');
+  //   expect(response.body[0].age).toEqual(42);
+  //   expect(response.body[0].pronouns).toEqual('they/them');
+  // });
 
 });
