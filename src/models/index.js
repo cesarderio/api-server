6d03cc04e2683bf71ffda9d35fd7e4014438af17
Raphael Chookagian <raphaelchookagian@gmail.com>
2022-12-01 16:45:07 -0800

    working on lab3 class lab and deployment again

--- src/models/index.js
+++ src/models/index.js
@@ -11,11 +11,11 @@ const ModelInterface = require('./modelInterface');
 // 'postgres://localhost:5432/api-app'
 // 'postgres://username:password@localhost:5432/api-app' <-- if you have a username and password
 // will use ternary here to set up sqlite for testing
-// const DATABASE_URL = process.env.DATABASE_URL;
+const DATABASE_URL = process.env.DATABASE_URL;
 
-const DATABASE_URL = process.env.NODE_ENV === 'test'
-  ? 'sqlite:memory'
-  : process.env.DATABASE_URL;
+// const DATABASE_URL = process.env.NODE_ENV === 'test'
+//   ? 'sqlite:memory'
+//   : process.env.DATABASE_URL;
 
 
 // instantiate our sequelize connection to our database
